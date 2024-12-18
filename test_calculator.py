9ff5704f46d6f783cd97dacad8d444734319ef4b
jiajingbin <jiajaybin@126.com>
2024-12-18 15:13:55 +0800

    test: push

--- test_calculator.py
+++ test_calculator.py
@@ -2,8 +2,8 @@ import unittest
 from calculator import add
 
 class TestCalculator(unittest.TestCase):
-def test_add(self):
-self.assertEqual(add(2, 3), 5)
+    def test_add(self):
+        self.assertEqual(add(2, 3), 5)
 
-if name == 'main':
-unittest.main()"
+if __name__ == '__main__':
+    unittest.main()
